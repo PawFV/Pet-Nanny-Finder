43b877bd2f4dbc3de1dac389a11e3432f6d3e1bd
Pau Joan <pregonipaw@hotmail.com>
2020-12-25 20:16:43 +0100

    generated mock data

---
+++ src/mocks/users.ts
@@ -0,0 +1,62 @@
+export default [
+  {
+    'id': '9c6c0076-e0b5-4f0e-822b-21d709daeed4',
+    'email': 'Eladio52@yahoo.com',
+    'name': 'Aidan40',
+    'password': 'b_C31ATFXtC0_hq'
+  },
+  {
+    'id': '9d2da46c-e3b5-41e3-b513-c6928747c526',
+    'email': 'Angie_Ebert@yahoo.com',
+    'name': 'Georgiana.Tillman99',
+    'password': 'by0ywe2j4fWGyXM'
+  },
+  {
+    'id': '6464749b-e9c6-4708-8424-84bea44f34f3',
+    'email': 'Burdette21@yahoo.com',
+    'name': 'Meredith.Marks',
+    'password': 'gh2qQiPR0DugS9R'
+  },
+  {
+    'id': '8abb6d7c-75ff-4d2c-a741-da2e90b0cc59',
+    'email': 'Francesco.Stiedemann@hotmail.com',
+    'name': 'Oscar_Hoppe12',
+    'password': '3vYsokFftnCMgNO'
+  },
+  {
+    'id': '2551618b-5efb-4e0a-ab98-2d4321f25639',
+    'email': 'Faye_Buckridge@hotmail.com',
+    'name': 'Jamar_Willms59',
+    'password': 'wAVhfBnHf1vYM2W'
+  },
+  {
+    'id': '7097368e-6c2a-4c10-8ce1-048498f2e474',
+    'email': 'Sherwood.Haag@hotmail.com',
+    'name': 'Dawn_Halvorson',
+    'password': 'BLvf4Ugd_QGoMGf'
+  },
+  {
+    'id': 'bd49451d-1280-4d17-b6cb-d4af3c8fafc6',
+    'email': 'Keaton_Effertz13@hotmail.com',
+    'name': 'John_Larkin',
+    'password': 'S6x0fiJy809pDT7'
+  },
+  {
+    'id': '255a66e6-1ab4-4689-9942-443771eaed45',
+    'email': 'Donna.Schumm@gmail.com',
+    'name': 'Mitchell.Oberbrunner67',
+    'password': '3Wv6JvpRTHdhKTA'
+  },
+  {
+    'id': '0a820262-cd4d-462f-8789-44af6e59a235',
+    'email': 'Maurice51@hotmail.com',
+    'name': 'Laurianne.Schmidt',
+    'password': 'D0L5iJJPgwxrB1Z'
+  },
+  {
+    'id': 'adebeb00-4d04-4c58-9498-bc536ac10601',
+    'email': 'Julia_Luettgen27@yahoo.com',
+    'name': 'Wilma.Kuhlman',
+    'password': '995V6unnTZY40nH'
+  }
+]
