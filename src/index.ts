bdf860f81107a633b55b88f09aa8ce9b1dcf7182
Pau Joan <pregonipaw@hotmail.com>
2020-12-25 20:15:12 +0100

    created root file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hi')
+/// <reference types="./global" />
+import { ApolloServer } from 'apollo-server-express'
+import express from 'express'
+import { resolvers, typeDefs } from './graphql'
+
+const app = express()
+
+new ApolloServer({ typeDefs, resolvers: resolvers as any }).applyMiddleware({
+  app
+})
+
+app.listen({ port: 3500 }, () =>
+  console.log(`🚀 Server ready at http://localhost:3500/graphql`)
+)
