cd4b354e21a31f39c9d7aba18bab0249689d0887
Pau Joan <pregonipaw@hotmail.com>
2020-12-26 01:40:43 +0100

    created logger for server.

---
+++ src/utils/log/logServer.ts
@@ -0,0 +1,12 @@
+import chalkColors from './chalkColors'
+const { yellow, cyan } = chalkColors()
+
+export default function logServer(port: string | number) {
+  console.log(
+    cyan(
+      `⚡ [Apollo Server] ready at -> ${yellow(
+        `http://localhost:${port}/graphql`
+      )}`
+    )
+  )
+}
