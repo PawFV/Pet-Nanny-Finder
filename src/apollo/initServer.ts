11eb174779d6b6d58c1ddc30571ac1a5a096364a
Pau Joan <pregonipaw@hotmail.com>
2020-12-27 13:06:02 +0100

    added introspection true to apolloserver
    silent

--- src/apollo/initServer.ts
+++ src/apollo/initServer.ts
@@ -1,6 +1,7 @@
-import { resolvers, typeDefs } from '../graphql/'
 import { ApolloServer } from 'apollo-server-express'
+import { resolvers, typeDefs } from '../graphql/'
 
-const initApolloServer = () => new ApolloServer({ typeDefs, resolvers })
+const initApolloServer = () =>
+  new ApolloServer({ introspection: true, playground: true, typeDefs, resolvers })
 
 export default initApolloServer
