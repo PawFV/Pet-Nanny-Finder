a3d057276b6810f11647396643e795e9318c6051
Pau Joan <pregonipaw@hotmail.com>
2020-12-27 12:14:20 +0100

    commented global .d.ts replaced by autogen code for the moment

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,53 +1,57 @@
-interface User {
-  id: string
-  email: string
-  password: string
-  name: string
-  reputation?: Reputation
-  location?: string
-  phone?: string
-  avatar?: string
-  chat?: Chat
-  reviews?: Reviews
-}
+// interface User {
+//   id: string
+//   email: string
+//   password: string
+//   name: string
+//   reputation?: Reputation | number
+//   location?: GeoLocation
+//   phone?: string
+//   avatar?: string
+//   chat?: Chat
+//   reviews?: Reviews
+// }
 
-interface Owner extends User {
-  pet?: Pet
-}
+// interface Owner extends User {
+//   pet?: Pet
+// }
 
-type Reputation = 0 | 1 | 2 | 3 | 4 | 5
+// interface GeoLocation {
+//   latitude: string
+//   longitud: string
+// }
+// type Reputation = 0 | 1 | 2 | 3 | 4 | 5
 
-// interface Carer extends User { }
+// // interface Carer extends User { }
 
-interface Reviews {
-  given: Review[]
-  received: Review[]
-}
+// interface Reviews {
+//   given: Review[]
+//   received: Review[]
+// }
 
-interface Review {
-  id: string
-  userId: string
-  text: string
-}
+// interface Review {
+//   id: string
+//   userId: string
+//   text: string
+// }
 
-type PetType = 'dog' | 'cat'
+// type PetType = 'dog' | 'cat'
 
-interface Pet {
-  id: string
-  ownerId: string
-  age: number
-  size: string
-  type: PetType
-  description: string
-  image: string
-}
+// interface Pet {
+//   id: string
+//   ownerId: string
+//   age: number
+//   size: string
+//   type: PetType
+//   description: string
+//   image: string
+// }
 
-interface Chat {
-  id: string
-  messages: ChatMessage[]
-}
+// interface Chat {
+//   id: string
+//   messages: ChatMessage[]
+// }
 
-interface ChatMessage {
-  userId: string
-  message: string
-}
+// interface ChatMessage {
+//   userId: string
+//   message: string
+// }
