883c1bbf0463c373ddf0bf565d0fd6fb6b24f4bf
Pau Joan <pregonipaw@hotmail.com>
2020-12-27 12:15:31 +0100

    updated config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,21 +2,16 @@ import dotenv from 'dotenv'
 import express from 'express'
 import 'module-alias/register'
 import initApolloServer from '../apollo/initServer'
+import { setDefaultLogger } from '../utils/log/logger'
 
-const configs = {
-  dotenv: () => {
-    dotenv.config()
-    return config()
-  },
-  defaults: () => {
-    return {
-      app: express(),
-      PORT: process.env.PORT || 3500,
-      server: initApolloServer()
-    }
+const config = () => {
+  dotenv.config()
+  setDefaultLogger()
+  return {
+    app: express(),
+    PORT: process.env.PORT || 3500,
+    apolloServer: initApolloServer()
   }
 }
-
-const config = () => configs
 
 export default config
