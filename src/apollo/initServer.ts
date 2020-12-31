bc1b596b47e8dedaf044b5b39b04d444a4a2f037
Pau <pregonipaw@hotmail.com>
2020-12-31 15:18:06 +0100

    refactored apollo server init

--- src/apollo/initServer.ts
+++ src/apollo/initServer.ts
@@ -1,7 +1,10 @@
 import { ApolloServer } from 'apollo-server-express'
 import { resolvers, typeDefs } from '../graphql/'
 
-const initApolloServer = () =>
-  new ApolloServer({ introspection: true, playground: true, typeDefs, resolvers })
-
-export default initApolloServer
+export default () =>
+  new ApolloServer({
+    introspection: true,
+    playground: true,
+    typeDefs,
+    resolvers
+  })
