31f66c007e97f2d31060f279916ee5c5bd8ac00e
Pau <pregonipaw@hotmail.com>
2020-12-31 10:37:01 +0100

    refactored and implemented relationships in reviews

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -5,36 +5,31 @@ import users from '../../database/users.json'
 
 const Query: QueryResolvers = {
   users: () => users,
-  user: (_, { id }) => {
-    const user = users.find(u => u.id === id)
-    return user
-  },
+  user: (_, { id }) => users.find(u => u.id === id),
   pets: () => pets,
-  review: (_, { id }) => reviews.find(r => id === r.id),
+  review: (_, { id }) => reviews.find(review => id === review.id),
   reviews: () => reviews
 }
 
 const Mutation: MutationResolvers = {
-  createUser: (_, { name, email, password }) => {
-    return {
-      id: '123',
-      name,
-      email,
-      password,
-      reviews: {}
-    }
-  },
-  createPet: (_, { pet }) => pets[0]
+  createUser: (_, args) => ({ ...args, id: '123' }),
+  createPet: () => pets[0]
 }
 
 const resolvers: Resolvers = {
   User: {
     pet: parent => pets.filter(pet => pet.ownerId === parent.id),
-    givenReviews: parent => reviews.filter(r => r.from === parent.id),
-    receivedReviews: parent => reviews.filter(r => r.to === parent.id)
+    givenReviews: parent => reviews.filter(review => review.from === parent.id),
+    receivedReviews: parent => reviews.filter(review => review.to === parent.id)
   },
   Pet: {
     owner: parent => users.find(user => user.id === parent.ownerId)
+  },
+  Review: {
+    users: parent => ({
+      from: users.find(user => user.id === parent.from),
+      to: users.find(user => user.id === parent.to)
+    })
   },
   Query,
   Mutation
