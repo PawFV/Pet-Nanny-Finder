5659de1b4ed4f3c5464620876e6178979a285f9b
Pau Joan <pregonipaw@hotmail.com>
2021-01-01 06:41:13 +0100

    updated to lodash methods for random numbers

--- src/mocks/gen/petData.ts
+++ src/mocks/gen/petData.ts
@@ -1,4 +1,5 @@
 import { random } from 'faker'
+import { random as randomNumber } from 'lodash'
 import { AvatarGenerator } from 'random-avatar-generator'
 import { Pet, User } from '../../utils/codegen/graphql'
 const avatar = new AvatarGenerator()
@@ -13,8 +14,8 @@ export default (users: User[]) => {
       description: random.words(5),
       image: avatar.generateRandomAvatar(),
       type: 'dog',
-      age: Math.round(Math.random() * 10),
-      size: Math.round(Math.random() * 50 + 10)
+      age: randomNumber(0, 10),
+      size: randomNumber(10, 50)
     })
   })
   return pets
